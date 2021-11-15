bdf2637f5b98c22af50ff3e388b2d023c96ba785
#TSA <tomysatriaalasi@gmail.com>
2021-11-15 11:58:30 +0700

    Create #2 Aritmatika.pas

---
+++ #2 Aritmatika.pas
@@ -0,0 +1,16 @@
+program aritmatika;
+VAR
+a, b : integer;
+begin
+    a:=4;
+    b:=2;
+    WRITELN('a=',a);
+WRITELN('b=',b);
+WRITELN('a+b=',a+b);
+WRITELN('a-b=',a-b);
+WRITELN('a*b=',a*b);
+WRITELN('a/b=',a/b);
+WRITELN('a div b=',a DIV b); { used with integers only }
+WRITELN('a mod b=',a MOD b);
+readln;
+end.
